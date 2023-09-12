0f653bd104e55157effb9d88abc0c98b6807f989
Mohamed-Z-Ali <mz355604@gmail.com>
2023-09-12 13:30:51 +0300

    sf

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+*main - Prints all single digits of base 16 followed by new line
+*
+*Return: returns 0
+*/
+int main(void)
+{
+for (char a = '0'; a <= '9'; a++)
+{
+putchar(a);
+if (a == '9')
+{
+char a = 'a';
+for (; a <= 'f'; a++)
+{
+putchar(a);
+}
+}
+}
+putchar('\n');
+return (0);
+}
