32e06e7eabb56aeae325344019b9b0fe371cd829
Mohamed-Z-Ali <mz355604@gmail.com>
2023-09-12 15:21:29 +0300

    fg

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,15 @@
 */
 int main(void)
 {
-	int digit;
-
-	for (digit = '0'; digit <= '9'; digit++)
-	{
-		putchar(digit);
-		if (digit == '9')
-			continue;
-
-		putchar(',');
-		putchar(' ');
-	}
-	putchar('\n');
-	return (0);
+int digit;
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+if (digit == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
 }
