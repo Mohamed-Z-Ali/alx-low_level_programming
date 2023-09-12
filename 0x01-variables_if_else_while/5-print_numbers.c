4cec161b5212775701d28a1b53199f8e62fb9b55
Mohamed-Z-Ali <mz355604@gmail.com>
2023-09-12 12:55:59 +0300

    ;lk

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+*main - Prints all single digits of base 10 followed by new line
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+int moa;
+for (moa = 0; moa < 10; moa++)
+{
+printf("%d", moa);
+
+}
+printf("\n");
+return (0);
+}
