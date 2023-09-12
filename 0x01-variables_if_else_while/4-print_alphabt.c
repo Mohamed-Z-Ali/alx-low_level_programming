84034dcdaa084ec57ae382065cb180e76aff90a1
Mohamed-Z-Ali <mz355604@gmail.com>
2023-09-12 12:46:34 +0300

    asd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+*main - Program to print alphabet letters except q and e followed by new line
+*
+*Return: return 0
+*/
+
+int main(void)
+{
+char hoss;
+for (hoss = 'a' ; hoss < 'z'; hoss++)
+{
+if (hoss == 'e' || hoss == 'q')
+{
+continue;
+}
+putchar(hoss);
+}
+putchar('\n');
+return (0);
+
+}
