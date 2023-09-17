db4a649a28c20e6b835edef6404f6ef36561d7af
Mohamed-Z-Ali <mz355604@gmail.com>
2023-09-18 01:30:36 +0300

    all togther

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - print any integer.
+ *
+ * @n: integer for print.
+ */
+
+void print_number(int n)
+{
+unsigned int d = n;
+
+if (n < 0)
+{
+putchar('-');
+d = -d;
+}
+
+if (d > 9)
+print_number(d / 10);
+
+
+putchar((d % 10) + '0');
+
+}
