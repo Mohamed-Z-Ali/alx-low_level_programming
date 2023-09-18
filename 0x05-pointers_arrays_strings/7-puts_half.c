47604a0ebe2369e367cc6650bada57d25ca2a372
Mohamed-Z-Ali <mz355604@gmail.com>
2023-09-19 01:27:46 +0300

    yummi

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
+
+return (inc);
+}
+
+
+
+/**
+*puts_half - puts half of a string
+*@str: string
+*Return:void
+*/
+void puts_half(char *str)
+{
+int len = _strlen(str);
+
+if (len % 2 == 0)
+len = len / 2;
+
+else
+len = (len + 1) / 2;
+
+while (str[len] != '\0')
+{
+putchar(str[len]);
+len++;
+}
+putchar('\n');
+}
