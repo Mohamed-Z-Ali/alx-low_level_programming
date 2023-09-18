46ab747e225fb96e871828ef3a043ea52d594590
Mohamed-Z-Ali <mz355604@gmail.com>
2023-09-19 00:15:38 +0300

    fourTH

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+* _puts - prints a string.
+*
+* @str: pinter.
+*/
 void _puts(char *str)
 {
-    int a;
-    for(a = 0; str[a] != '\0'; a++)
-    {
-    putchar(str[a]);
-    }
-    putchar('\n');
+int a;
+for (a = 0; str[a] != '\0'; a++)
+{
+putchar (str[a]);
+}
+putchar ('\n');
 }
